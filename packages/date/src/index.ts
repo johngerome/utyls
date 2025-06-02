df65be6f8e8ec2e1ba5ac57871a2cfed0df587c6
0x4e32 <johngerome@pm.me>
2025-06-02 14:17:24 +0800

    fix(date): fix date utility exports (#7)
    * fix(date): fix date utility exports
    
    * fix: update package.json exports to use import instead of default field
    
    * fix: add utility functions for week and month date operations

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -3,3 +3,6 @@ export * from "./toShortDate.js";
 export * from "./toFullDate.js";
 export * from "./toISODate.js";
 export * from "./toFormalDate.js";
+export * from "./getCurrentWeek.js";
+export * from "./getMonthToday.js";
+export * from "./getTotalWeeksInMonth.js";
